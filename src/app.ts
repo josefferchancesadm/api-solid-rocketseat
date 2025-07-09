eb6836fee3248e32b8102800ae340c5fec7123c1
josefferchancesadm <josefferchancesadm@gmail.com>
2025-07-09 16:42:13 -0300

    inicio schema do prisma

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,15 @@
 import fastify from "fastify";
 
+import { PrismaClient } from "generated/prisma";
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "John Doe",
+    email: "john.doe@example.com"    
+  }
+});
+
 export const app = fastify();
 
