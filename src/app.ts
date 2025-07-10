f7c196e698db48fee9fd4df3fe4c7c2f14f68175
josefferchancesadm <josefferchancesadm@gmail.com>
2025-07-10 14:15:37 -0300

    create relationships

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,10 @@
 import fastify from "fastify";
 
-import { PrismaClient } from "generated/prisma";
+// import { PrismaClient } from "generated/prisma";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-prisma.user.create({
-  data: {
-    name: "John Doe",
-    email: "john.doe@example.com"    
-  }
-});
+
 
 export const app = fastify();
 
